8ad68409316c8347ac6762b32d77986ed7baea11
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-05-02 13:18:48 +0200

    Improve flatten utility to support recursive operation

--- ues/transform/util.py
+++ ues/transform/util.py
@@ -31,5 +31,13 @@ def dict_value(dictionary: Dict[Any, Any]) -> Any:
     return next(iter(vals))
 
 
-def flatten(deep_lst: List[List[Any]]) -> List[Any]:
-    return list(itertools.chain(*deep_lst))
+def flatten(deep_lst: List[List[Any]], *, recursive=False) -> List[Any]:
+    deep_lst = [[deep_elem] if not isinstance(deep_elem, list) else deep_elem for deep_elem in deep_lst]
+    flattened = list(itertools.chain(*deep_lst))
+    if recursive and any(isinstance(deep_elem, list) for deep_elem in flattened):
+        return flatten(flattened, recursive=True)
+    return flattened
+
+
+def enlist(obj: Any) -> List[Any]:
+    return obj if isinstance(obj, list) else [obj]
