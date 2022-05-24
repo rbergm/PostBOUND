d02b554facd55f56dcd189e682adf17ba7dfe39f
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-05-24 18:24:15 +0200

    Integrate flexible IdxNLJ generation settings in CLI utility

--- ues/subquery-hinting.py
+++ ues/subquery-hinting.py
@@ -20,6 +20,12 @@ def main():
     parser.add_argument("--mode", "-m", action="store", default="ues-idxnlj", help="The kind of hints to produce. "
                         "Currently only 'ues-idxnlj' (the default) is supported, which enforces an "
                         "Index-NestedLoopJoin in UES subqueries queries.", choices=["ues-idxnlj"])
+    parser.add_argument("--idx-target", action="store", default="fk", choices=["pk", "fk"], help="For "
+                        "'ues-idxnlj'-mode: The subquery join-partner that should be implemented as IndexScan. Can be "
+                        "either 'pk' or 'fk', denoting the Primary key table and Foreign key table, respectively.")
+    parser.add_argument("--nlj-scope", action="store", default="first", choices=["first", "all"], help=" For "
+                        "'ues-idxnlj'-mode: How many Index-Nested loop joins should be generated. Can be either "
+                        "'first' denoting only the innermost join, or 'all', denoting all joins.")
     parser.add_argument("--out", "-o", action="store", default="out.csv", help="Name of the CSV file to store the "
                         "output")
     parser.add_argument("--query-col", action="store", default="query", help="Name of the CSV column containing the "
@@ -30,7 +36,7 @@ def main():
     args = parser.parse_args()
     df = read_input(args.input, args.query_col)
     df[args.hint_col] = (df[args.query_col]
-                         .apply(hint.idxnlj_subqueries)
+                         .apply(hint.idxnlj_subqueries, nestloop=args.nlj_scope, idxscan=args.nlj_scope)
                          .apply(hint.HintedMospQuery.generate_sqlcomment, strip_empty=True))
 
     df.to_csv(args.out, index=False)
