798c1bf64364a9369288cee9f3fa2fae60798ae0
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-07-24 13:30:19 +0200

    UES and hinting tests baseline

---
+++ ues/tests/test_hinting.py
@@ -0,0 +1,11 @@
+
+import unittest
+
+
+class IdxNLJHintingTests(unittest.TestCase):
+    def test_base_hinting(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ ues/tests/test_ues.py
@@ -0,0 +1,29 @@
+
+import unittest
+
+
+class BeningQueryOptimizationTests(unittest.TestCase):
+    def test_base_query(self):
+        pass
+
+
+class SnowflaxeQueryOptimizationTests(unittest.TestCase):
+    def test_base_query(self):
+        pass
+
+
+class CrossProductQueryOptimizationTests(unittest.TestCase):
+    def test_base_query(self):
+        pass
+
+    def test_base_with_snowflake(self):
+        pass
+
+
+class WeirdQueriesOptimizationTests(unittest.TestCase):
+    def test_no_joins_query(self):
+        pass
+
+
+if "__name__" == "__main__":
+    unittest.main()
