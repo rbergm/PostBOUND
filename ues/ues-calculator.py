2c94a27a3dd1ba91158b8b4a740d95a6bdacb304
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-08-23 12:05:49 +0200

    Add verbose mode to UES calculator

--- ues/ues-calculator.py
+++ ues/ues-calculator.py
@@ -9,13 +9,21 @@ def main():
     parser.add_argument("-tb", type=int, required=True, help="Total number of tuples in relation B")
     parser.add_argument("-mfa", type=int, required=True, help="Maximum attribute frequency of A's join attribute")
     parser.add_argument("-mfb", type=int, required=True, help="Maximum attribute frequency of B's join attribute")
+    parser.add_argument("--verbose", "-v", action="store_true", default=False, help="Also print distinct value counts")
 
     args = parser.parse_args()
     da = args.ta / args.mfa
     db = args.tb / args.mfb
 
     bound = min(da, db) * args.mfa * args.mfb
-    print(round(bound))
+    bound = round(bound)
+
+    if args.verbose:
+        print(" distinct(a) =", da)
+        print(" distinct(b) =", db)
+        print("upper(A ⋈ B) =", bound)
+    else:
+        print(bound)
 
 
 if __name__ == "__main__":
