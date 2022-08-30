f8c260be78207c07bdedd3547bbfe1add7adc6bf
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-08-30 17:28:36 +0200

    Get rid of SDR subrepo, only use JOB queries

--- ues/tests/regression_suite.py
+++ ues/tests/regression_suite.py
@@ -5,7 +5,7 @@ import pathlib
 from typing import Dict
 
 
-def load_job_workload(path: str = "../../simplicity-done-right/JOB-Queries/implicit",
+def load_job_workload(path: str = "../../workloads/JOB-Queries/implicit",
                       source_pattern: str = "*.sql") -> Dict[str, str]:
     root = pathlib.Path(path)
     workload_files = list(root.glob(source_pattern))
--- ues/transform/job.py
+++ ues/transform/job.py
@@ -4,7 +4,7 @@ import pathlib
 from transform import mosp
 
 
-def load_query(label: str, *, src_dir: str = "../simplicity-done-right/JOB-Queries/implicit") -> mosp.MospQuery:
+def load_query(label: str, *, src_dir: str = "../workloads/JOB-Queries/implicit") -> mosp.MospQuery:
     fname = f"{label}.sql"
     path = pathlib.Path(src_dir) / fname
     with open(path, "r") as query_file:
