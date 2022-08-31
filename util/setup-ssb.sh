a0a8f1e6923c061688801d6a787d9d8a7b39029d
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-08-31 12:47:35 +0200

    Small SSB setup fixes

--- util/setup-ssb.sh
+++ util/setup-ssb.sh
@@ -3,18 +3,19 @@
 root=$(pwd)
 
 echo ".. Building TPC-H utility"
-cd tpch-kit/dbgen
+patch ssb-kit/dbgen/bm_utils.c tpch_dbgen.patch
+cd ssb-kit/dbgen
 make MACHINE=LINUX DATABASE=POSTGRESQL
 
 cd $root
 echo ".. Setting up environment"
-export DSS_CONFIG=$(pwd)/tpch-kit/dbgen
+export DSS_CONFIG=$(pwd)/ssb-kit/dbgen
 export DSS_QUERY=$DSS_CONFIG/queries
 export DSS_PATH=../tpch_data
 
 echo ".. Generating TPCH data (SF = 0.5)"
 mkdir ../tpch_data
-tpch-kit/dbgen/dbgen -vf -s 0.5
+ssb-kit/dbgen/dbgen -vf -s 0.5 -T a
 
 echo ".. Inserting TPCH data into database"
 wget -nv --output-document ../tpch_data/pg_schema.sql https://raw.githubusercontent.com/gregrahn/ssb-kit/master/scripts/pg_schema.sql
