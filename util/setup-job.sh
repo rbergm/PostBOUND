a0e95aefb4f1ae17c86de4c4ad512061594b3ebd
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-11-23 09:09:10 +0100

    Replace Python-based IMDB setup by pure bash version

---
+++ util/setup-job.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+PWD=$(pwd)
+if [ -z "$1" ] ; then
+    IMDB_DIR="../imdb_data"
+else
+    IMDB_DIR="$1"
+fi
+
+echo ".. IMDB source directory is $IMDB_DIR"
+
+if [ -d $IMDB_DIR ] ; then
+    echo ".. Re-using existing IMDB input data"
+else
+    echo ".. IMDB source directory does not exist, re-creating"
+    echo ".. Fetching IMDB data"
+    mkdir $IMDB_DIR
+    curl -o $IMDB_DIR/csv.zip "https://cloudstore.zih.tu-dresden.de/index.php/s/eqWWK53CgkxMxfA/download?path=%2F&files=csv.zip"
+    curl -o $IMDB_DIR/create.sql "https://cloudstore.zih.tu-dresden.de/index.php/s/eqWWK53CgkxMxfA/download?path=%2F&files=create.sql"
+    curl -o $IMDB_DIR/import.sql "https://cloudstore.zih.tu-dresden.de/index.php/s/eqWWK53CgkxMxfA/download?path=%2F&files=import.sql"
+    curl -o $IMDB_DIR/fkindexes.sql "https://cloudstore.zih.tu-dresden.de/index.php/s/eqWWK53CgkxMxfA/download?path=%2F&files=fkindexes.sql"
+
+    echo ".. Extracting IMDB data"
+    unzip $IMDB_DIR/csv.zip -d $IMDB_DIR
+fi
+
+echo ".. Creating IMDB database schema"
+createdb imdb
+psql imdb -f $IMDB_DIR/create.sql
+
+echo ".. Inserting IMDB data into database"
+cd $IMDB_DIR
+psql imdb -f import.sql
+
+echo ".. Creating IMDB foreign key indices"
+psql imdb -f fkindexes.sql
+psql imdb -c "create index if not exists subject_id_complete_cast on complete_cast(subject_id)"
+psql imdb -c "create index if not exists status_id_complete_cast on complete_cast(status_id)"
+
+echo ".. Done"
+cd $PWD
