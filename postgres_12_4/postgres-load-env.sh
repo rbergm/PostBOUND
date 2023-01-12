f8785a4373e14e28f1b2aa29f567c79673659a85
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-12 14:38:37 +0100

    Fix lazy PG env loading condition

--- postgres_12_4/postgres-load-env.sh
+++ postgres_12_4/postgres-load-env.sh
@@ -5,7 +5,7 @@ cd postgres-server
 PG_CTL_PATH="$(pwd)/build/bin"
 INIT=$(echo "$PATH" | grep "$PG_CTL_PATH")
 
-if [ -z $INIT ] ; then
+if [ -z "$INIT" ] ; then
 	export PG_CTL_PATH
 	export PATH="$PG_CTL_PATH:$PATH"
 	export LD_LIBRARY_PATH="$(pwd)/build/lib:$LD_LIBRARY_PATH"
