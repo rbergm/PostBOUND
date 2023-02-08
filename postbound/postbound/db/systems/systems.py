aa41c8f94910b7d5d7c29ff85fd40cbb991836f8
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-02-08 15:45:15 +0100

    Remove hints from SELECT clause again
    Instead, hints are now only available as a top-level clause of SQL
    queries. To accomodate for database system-specific placement of hints,
    the functionality to format queries has been added to DB systems.

--- postbound/postbound/db/systems/systems.py
+++ postbound/postbound/db/systems/systems.py
@@ -4,6 +4,7 @@ import abc
 
 from postbound.db import db
 from postbound.db.hints import provider as hint_provider
+from postbound.qal import qal
 
 
 class DatabaseSystem(abc.ABC):
@@ -14,6 +15,10 @@ class DatabaseSystem(abc.ABC):
 
     @abc.abstractmethod
     def query_adaptor(self) -> hint_provider.HintProvider:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def format_query(self, query: qal.SqlQuery) -> str:
         raise NotImplementedError
 
 
