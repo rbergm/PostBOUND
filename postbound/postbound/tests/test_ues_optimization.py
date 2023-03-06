8f7abc89e11e729c2692925d05f158e73a9db5d3
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-03-06 16:03:34 +0100

    Don't execute queries that can't be optimized

--- postbound/postbound/tests/test_ues_optimization.py
+++ postbound/postbound/tests/test_ues_optimization.py
@@ -63,18 +63,18 @@ class StackWorkloadTests(regression_suite.DatabaseTestCase):
         self.db = postgres.connect(config_file=f"{pg_connect_dir}/.psycopg_connection_stack")
         self.db.statistics().emulated = True
         self.db.statistics().cache_enabled = True
-        self.ssb = workloads.ssb()
+        self.stack = workloads.stack()
 
     def test_optimize_workload(self) -> None:
         optimization_pipeline = pb.OptimizationPipeline(target_dbs=systems.Postgres(self.db))
         optimization_pipeline.load_settings(presets.fetch("ues"))
         optimization_pipeline.build()
-        for label, query in self.ssb.entries():
+        for label, query in self.stack.entries():
             # Stack is only partially supported by UES
             with self.subTest(label=label, query=query):
                 try:
-                    original_result = self.db.execute_query(query, cache_enabled=False)
                     optimized_query = optimization_pipeline.optimize_query(query)
+                    original_result = self.db.execute_query(query, cache_enabled=False)
                     optimized_result = self.db.execute_query(optimized_query, cache_enabled=False)
                     self.assertResultSetsEqual(original_result, optimized_result, ordered=query.is_ordered())
                 except validation.UnsupportedQueryError as e:
