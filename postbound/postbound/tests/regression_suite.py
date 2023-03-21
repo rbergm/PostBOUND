8330634883cb25f9acc90488aff5da32d53281fc
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-03-21 10:58:01 +0100

    Improve result set display for database test cases

--- postbound/postbound/tests/regression_suite.py
+++ postbound/postbound/tests/regression_suite.py
@@ -22,31 +22,48 @@ def _rebuild_result_set(result_set: object) -> list[tuple[object]]:
     return [(value,) for value in result_set]
 
 
+def _stringify_result_set(result_set: list[tuple[object]]) -> str:
+    if not "__len__" in dir(result_set):
+        return str(result_set)
+    if len(result_set) > 5:
+        shortened_result_set = result_set[:5]
+        return f"result set ({len(result_set)} elements total) :: first 5 = {shortened_result_set}"
+    return f"result set ({len(result_set)} elements) :: contents = {result_set}"
+
+
 def _assert_default_result_sets_equal(first_set: list[tuple[object]], second_set: list[tuple[object]]) -> None:
-    first_set = set(first_set)
-    second_set = set(second_set)
-    if first_set != second_set:
-        raise AssertionError(f"Result sets differ: {first_set} vs. {second_set}")
+    first_set_set = set(first_set)
+    second_set_set = set(second_set)
+    if first_set_set != second_set_set:
+        first_set_str = _stringify_result_set(first_set)
+        second_set_str = _stringify_result_set(second_set)
+        raise AssertionError(f"Result sets differ: {first_set_str} vs. {second_set_str}")
 
 
 def _assert_ordered_result_sets_equal(first_set: list[tuple[object]], second_set: list[tuple[object]]) -> None:
     for cursor, row in enumerate(first_set):
         comparison_row = second_set[cursor]
         if row != comparison_row:
-            raise AssertionError(f"Result sets differ: {first_set} vs {second_set}")
+            first_set_str = _stringify_result_set(first_set)
+            second_set_str = _stringify_result_set(second_set)
+            raise AssertionError(f"Result sets differ: {first_set_str} vs {second_set_str}")
 
 
 class DatabaseTestCase(unittest.TestCase, abc.ABC):
     def assertResultSetsEqual(self, first_set: object, second_set: object, *, ordered: bool = False) -> None:
         if type(first_set) != type(second_set):
             error_msg = "Result sets have different types: "
-            error_msg += f"{first_set} ({type(first_set)}) and {second_set} ({type(second_set)})"
+            first_set_str = _stringify_result_set(first_set)
+            second_set_str = _stringify_result_set(second_set)
+            error_msg += f"{first_set_str} ({type(first_set)}) and {second_set_str} ({type(second_set)})"
             raise AssertionError(error_msg)
 
         first_set = _rebuild_result_set(first_set)
         second_set = _rebuild_result_set(second_set)
         if len(first_set) != len(second_set):
-            raise AssertionError(f"Result sets have different length: {first_set} and {second_set}")
+            first_set_str = _stringify_result_set(first_set)
+            second_set_str = _stringify_result_set(second_set)
+            raise AssertionError(f"Result sets have different length: {first_set_str} and {second_set_str}")
 
         if ordered:
             _assert_ordered_result_sets_equal(first_set, second_set)
