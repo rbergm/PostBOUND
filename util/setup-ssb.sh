d4b3f5c35c6e584b10283f02fd9f75fe7b0e3a64
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-04-23 10:36:25 +0200

    Rename all mentions of TPCH to SSB
    Because we are working with the SSB which modifies the TPCH data
    slightly.

--- util/setup-ssb.sh
+++ util/setup-ssb.sh
@@ -9,8 +9,8 @@ fi
 
 root=$(pwd)
 
-echo ".. Building TPC-H utility"
-patch -fs ssb-kit/dbgen/bm_utils.c tpch_dbgen.patch
+echo ".. Building SSB utility"
+patch -fs ssb-kit/dbgen/bm_utils.c ssb_dbgen.patch
 cd ssb-kit/dbgen
 make MACHINE=LINUX DATABASE=POSTGRESQL
 
@@ -20,22 +20,22 @@ export DSS_CONFIG=$(pwd)/ssb-kit/dbgen
 export DSS_QUERY=$DSS_CONFIG/queries
 export DSS_PATH=../tpch_data
 
-echo ".. Generating TPCH data (SF = $SF)"
-mkdir -p ../tpch_data
+echo ".. Generating SSB data (SF = $SF)"
+mkdir -p ../ssb_data
 ssb-kit/dbgen/dbgen -vf -s $SF -T a
 
-echo ".. Creating TPC-H database schema"
-createdb tpch
-wget -nv --output-document ../tpch_data/pg_schema.sql https://raw.githubusercontent.com/gregrahn/ssb-kit/master/scripts/pg_schema.sql
-psql tpch -f ../tpch_data/pg_schema.sql
+echo ".. Creating SSB database schema"
+createdb ssb
+wget -nv --output-document ../ssb_data/pg_schema.sql https://raw.githubusercontent.com/gregrahn/ssb-kit/master/scripts/pg_schema.sql
+psql ssb -f ../ssb_data/pg_schema.sql
 
-echo ".. Inserting TPC-H data into database"
-wget -nv --output-document ../tpch_data/pg_load.sql https://raw.githubusercontent.com/gregrahn/ssb-kit/master/scripts/pg_load.sql
+echo ".. Inserting SSB data into database"
+wget -nv --output-document ../ssb_data/pg_load.sql https://raw.githubusercontent.com/gregrahn/ssb-kit/master/scripts/pg_load.sql
 
 # this patch may never fail since we just downloaded the live version (unless this version is changed)
-patch ../tpch_data/pg_load.sql tpch_pg_load.patch
-psql tpch -f ../tpch_data/pg_load.sql
+patch ../ssb_data/pg_load.sql ssb_pg_load.patch
+psql ssb -f ../ssb_data/pg_load.sql
 
 
 echo ".. Creating SSB Foreign Key indices"
-psql tpch -f tpch_fk_indexes.sql
+psql ssb -f tpch_fk_indexes.sql
