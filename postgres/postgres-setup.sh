443f1586b07c4b012c2cae5b042dd6c3b66f81fa
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-05-12 11:19:52 +0200

    Fix autoprewarm configuration in PG setup

--- postgres/postgres-setup.sh
+++ postgres/postgres-setup.sh
@@ -38,7 +38,7 @@ initdb -D $(pwd)/data
 
 echo "... Adding pg_hint_plan and pg_prewarm to preload libraries"
 sed -i "s/#\{0,1\}shared_preload_libraries.*/shared_preload_libraries = 'pg_hint_plan,pg_prewarm'/" $(pwd)/data/postgresql.conf
-echo "pg.prewarm = false" >>  $(pwd)/data/postgresql.conf
+echo "pg_prewarm.autoprewarm = false" >>  $(pwd)/data/postgresql.conf
 
 echo "... Starting Postgres (log file is pg.log)"
 pg_ctl -D $(pwd)/data -l pg.log start
