4f900e92e12c9dbea238b10210521ca748e74718
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-05-19 09:17:30 +0200

    Add support for native join order optimizers

---
+++ postbound/postbound/optimizer/strategies/native.py
@@ -0,0 +1,24 @@
+"""Provides access to the optimization decisions made by the native database system optimizer"""
+from __future__ import annotations
+
+from typing import Optional
+
+from postbound.db import db
+from postbound.qal import qal
+from postbound.optimizer import jointree
+from postbound.optimizer.joinorder import enumeration
+
+
+class NativeJoinOrderOptimizer(enumeration.JoinOrderOptimizer):
+
+    def __init__(self, db_instance: db.Database) -> None:
+        super().__init__("Native join ordering")
+        self.db_instance = db_instance
+
+    def optimize_join_order(self, query: qal.SqlQuery) -> Optional[jointree.LogicalJoinTree]:
+        # TODO: we could probably change the implementation at some later point in time to generate a physical QEP
+        query_plan = self.db_instance.optimizer().query_plan(query)
+        return jointree.LogicalJoinTree.load_from_query_plan(query_plan, query)
+
+    def describe(self) -> dict:
+        return {"name": "native", "database_system": self.db_instance.inspect()}
