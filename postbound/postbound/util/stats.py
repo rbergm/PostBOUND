f81debe77f3b4d11da37f7d6de889347d3080a8a
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-05-26 15:05:34 +0200

    Add hacky utility to create score matrices

--- postbound/postbound/util/stats.py
+++ postbound/postbound/util/stats.py
@@ -1,7 +1,12 @@
-"""Different mathematical and statistical formulas and utilties."""
+"""Different mathematical and statistical formulas and utilities."""
 from __future__ import annotations
 
 import math
+import numbers
+import typing
+from collections.abc import Callable, Iterable
+
+import numpy as np
 
 
 def catalan_number(n: int) -> int:
@@ -12,3 +17,18 @@ def catalan_number(n: int) -> int:
 def jaccard(a: set | frozenset, b: set | frozenset) -> float:
     """Jaccard coefficient between a and b. Defined as |a ∩ b| / |a ∪ b|"""
     return len(a & b) / len(a | b)
+
+
+T = typing.TypeVar("T")
+
+
+def score_matrix(elems: Iterable[T], scoring: Callable[[T, T], numbers.Number]) -> np.ndarray:
+    elems = list(elems)
+    n = len(elems)
+
+    matrix = np.ones((n, n))
+    for i, elem_i in enumerate(elems):
+        for j, elem_j in enumerate(elems):
+            matrix[i, j] = scoring(elem_i, elem_j)
+
+    return matrix
