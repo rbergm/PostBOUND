0b21eff97ea7e5c832af062ae24e0fbfed677937
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-05-26 12:14:31 +0200

    TONIC visualization improvements

--- postbound/postbound/vis/tonic.py
+++ postbound/postbound/vis/tonic.py
@@ -1,19 +1,24 @@
 
 from __future__ import annotations
 
+import random
 from typing import Optional
 
 import graphviz as gv
 
 from postbound.optimizer.strategies import tonic
 
+def _unique_node_identifier(identifier: tonic.QepsIdentifier) -> str:
+    return str(hash((identifier, random.random())))
+
+
 def _render_subquery_path(qeps: tonic.QepsNode, current_node: str, current_graph: gv.Digraph) -> None:
     for identifier, qeps_child in qeps.child_nodes.items():
         child_node = _make_node_label(identifier, qeps_child)
-        current_graph.node(child_node, style="dashed")
-        current_graph.edge(current_node, child_node, style="dashed")
-        _render_subquery_path(qeps_child, child_node, current_graph)
-
+        node_identifier = _unique_node_identifier(identifier)
+        current_graph.node(node_identifier, label=child_node, style="dashed")
+        current_graph.edge(current_node, node_identifier, style="dashed")
+        _render_subquery_path(qeps_child, node_identifier, current_graph)
 
 def _make_node_label(identifier: tonic.QepsIdentifier, node: tonic.QepsNode) -> str:
     cost_str = ("[" + ", ".join(f"{operator.value}={cost}" for operator, cost in node.operator_costs.items()) + "]"
@@ -22,7 +27,7 @@ def _make_node_label(identifier: tonic.QepsIdentifier, node: tonic.QepsNode) -> 
     return label + "\n" + cost_str
 
 
-def plot_tonic_qeps(qeps: tonic.QepsNode, *, _current_node: Optional[str],
+def plot_tonic_qeps(qeps: tonic.QepsNode, *, _current_node: Optional[str] = None,
                     _current_graph: Optional[gv.Digraph] = None) -> gv.Digraph:
     if not _current_graph:
         _current_graph = gv.Digraph()
@@ -32,12 +37,13 @@ def plot_tonic_qeps(qeps: tonic.QepsNode, *, _current_node: Optional[str],
 
     for identifier, qeps_child in qeps.child_nodes.items():
         child_node = _make_node_label(identifier, qeps_child)
+        node_identifier = _unique_node_identifier(identifier)
         if qeps_child.subquery_root:
-            _current_graph.node(child_node, style="dashed")
+            _current_graph.node(node_identifier, label=child_node, style="dashed")
         else:
-            _current_graph.node(child_node)
+            _current_graph.node(node_identifier, label=child_node)
         if _current_node:
-            _current_graph.edge(_current_node, child_node)
-        plot_tonic_qeps(qeps_child, _current_node=child_node, _current_graph=_current_graph)
+            _current_graph.edge(_current_node, node_identifier)
+        plot_tonic_qeps(qeps_child, _current_node=node_identifier, _current_graph=_current_graph)
 
     return _current_graph
