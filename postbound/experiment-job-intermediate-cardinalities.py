7d6cbae317fef1c0433431eb5ffb1d2aa0aace9f
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-06-02 16:48:28 +0200

    Directly store tables for intermediate JOB cardinalities

--- postbound/experiment-job-intermediate-cardinalities.py
+++ postbound/experiment-job-intermediate-cardinalities.py
@@ -8,13 +8,13 @@ import pandas as pd
 from postbound.qal import qal, transform
 from postbound.db import postgres
 from postbound.experiments import workloads, analysis
-from postbound.util import collections as collection_utils
+from postbound.util import collections as collection_utils, jsonize
 
 workloads.workloads_base_dir = "../workloads"
 
 postgres_db = postgres.connect(config_file=".psycopg_connection_job")
 db_pool = postgres.ParallelQueryExecutor(postgres_db.connect_string, n_threads=12)
-job_benchmark = workloads.job()
+job_benchmark = workloads.job(simplified=False)
 
 explored_queries: set[qal.SqlQuery] = set()
 fragment_to_queries_map: dict[qal.SqlQuery, list[qal.SqlQuery]] = collections.defaultdict(list)
@@ -35,7 +35,9 @@ for label, query in job_benchmark.entries():
         if not query_predicates.joins_tables(joined_tables):
             continue
 
-        query_fragment = transform.as_count_star_query(transform.extract_query_fragment(query, joined_tables))
+        query_fragment = transform.extract_query_fragment(query, joined_tables)
+        assert query_fragment is not None
+        query_fragment = transform.as_count_star_query(query_fragment)
         if query_fragment in explored_queries:
             continue
         explored_queries.add(query_fragment)
@@ -46,16 +48,18 @@ print(".. All queries submitted to DB pool")
 db_pool.drain_queue()
 fragment_cardinalities: dict[qal.SqlQuery, int] = db_pool.result_set()
 
-queries, fragments, labels, cardinalities = [], [], [], []
-for query_fragment, cardinality in fragment_cardinalities.items():
-    for query in fragment_to_queries_map[query_fragment]:
+queries, fragments, labels, fragment_tables, cardinalities = [], [], [], [], []
+for result_fragment, cardinality in fragment_cardinalities.items():
+    for query in fragment_to_queries_map[result_fragment]:
         query_label = job_benchmark.label_of(query)
         queries.append(query)
-        fragments.append(query_fragment)
+        fragments.append(result_fragment)
         labels.append(query_label)
+        fragment_tables.append(jsonize.to_json(list(result_fragment.tables())))
         cardinalities.append(cardinality)
 
 result_df = pd.DataFrame({"label": labels, "query": queries,
-                          "query_fragment": fragments, "cardinality": cardinalities})
+                          "query_fragment": fragments, "tables": fragment_tables,
+                          "cardinality": cardinalities})
 result_df = analysis.sort_results(result_df, "label")
 result_df.to_csv("job-intermediate-cardinalities.csv", index=False)
