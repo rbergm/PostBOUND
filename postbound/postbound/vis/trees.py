3576850e071d2928285f0c11da68d19f2d85e2a6
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-11-15 20:11:15 +0100

    Change tree visualization to use hash-based node identifiers
    This fixes rare instances where two different nodes would have the exact same
    label and therefore be linked up incorrectly.

--- postbound/postbound/vis/trees.py
+++ postbound/postbound/vis/trees.py
@@ -9,7 +9,8 @@ import graphviz as gv
 T = typing.TypeVar("T")
 
 
-def _gv_escape(content: str) -> str:
+def _gv_escape(node: T) -> str:
+    content = str(node) + str(hash(node))
     return "".join(c for c in content if c.isalnum())
 
 
@@ -17,11 +18,11 @@ def plot_tree(node: T, label_generator: Callable[[T], tuple[str, dict]], child_s
               __graph: Optional[gv.Graph] = None) -> gv.Graph:
     __graph = gv.Graph() if __graph is None else __graph
     label, params = label_generator(node)
-    node_key = _gv_escape(str(node))
-    __graph.node(gv.escape(node_key), label=label, **params)
+    node_key = _gv_escape(node)
+    __graph.node(node_key, label=label, **params)
 
     for child in child_supplier(node):
-        child_key = _gv_escape(str(child))
+        child_key = _gv_escape(child)
         __graph.edge(node_key, child_key)
         __graph = plot_tree(child, label_generator, child_supplier, __graph=__graph)
     return __graph
