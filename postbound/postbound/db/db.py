rbergm/PostBOUND

commit ace5ea3246ccdafe1dfe46a7e5f2d0777af9c61c

Fix Postgres GeQO management and restore behaviour
The old implementation only obtained the current GeQO state once during
creation of the Postgres interface and never updated this state. If some
external component modified the state in the meantime, these changes
would not be reflected in the current Postgres interface. Therefore,
after executing a query, the old GeQO state would be restored and the
external changes would be lost.

postbound/postbound/db/postgres.py
@@ -410,15 +410,17 @@ class PostgresInterface(db.Database):
 
     def execute_query(self, query: qal.SqlQuery | str, *, cache_enabled: Optional[bool] = None) -> Any:
         cache_enabled = cache_enabled or (cache_enabled is None and self._cache_enabled)
-        query = self._prepare_query_execution(query)
 
         if cache_enabled and query in self._query_cache:
             query_result = self._query_cache[query]
         else:
             try:
-                self._cursor.execute(query)
+                prepared_query = self._prepare_query_execution(query)
+                self._current_geqo_state = self._obtain_geqo_state()
+                self._cursor.execute(prepared_query)
                 query_result = self._cursor.fetchall() if self._cursor.rowcount >= 0 else None
-                self._restore_geqo_state()
+                if not _modifies_geqo_config(query):
+                    self._restore_geqo_state()
             except (psycopg.InternalError, psycopg.OperationalError) as e:
                 msg = "\n".join([f"At {utils.current_timestamp()}", "For query:", str(query), "Message:", str(e)])
                 raise db.DatabaseServerError(msg, e)

commit bfc398b87377b45389abbb647fb80c8a801df54e

Fix regression introduced in ace5ea3246ccdafe1dfe46a7e5f2d0777af9c61c
That commit differentiated between the original query object passed into
the execute_query() method and the object that will get executed in the
end. However, the original object was still stored in the query cache.
After the change, the cache could receive instances of SqlQuery instead.
In turn, this would break the JSON representation of the cache, since
json.dump only supports primitive objects.

postbound/postbound/db/db.py
@@ -31,7 +31,7 @@ from typing import Any, Literal, Optional
 
 from postbound.qal import base, parser, qal
 from postbound.optimizer import jointree, physops, planparams
-from postbound.util import collections as collection_utils, dicts as dict_utils, jsonize, misc
+from postbound.util import collections as collection_utils, dicts as dict_utils, misc
 
 
 class Cursor(typing.Protocol):
@@ -1199,7 +1199,7 @@ class HintService(abc.ABC):
         raise NotImplementedError
 
 
-class QueryExecutionPlan(jsonize.Jsonizable):
+class QueryExecutionPlan:
     """Heavily simplified system-independent model of physical query execution plans.
 
     A plan is a tree structure of `QueryExecutionPlan` objects. Each plan is a node in the tree and can have additional

postbound/postbound/db/postgres.py
@@ -424,15 +424,15 @@ class PostgresInterface(db.Database):
                 query_result = self._cursor.fetchall() if self._cursor.rowcount >= 0 else None
                 if not _modifies_geqo_config(query):
                     self._restore_geqo_state()
+                if cache_enabled:
+                    self._inflate_query_cache()
+                    self._query_cache[prepared_query] = query_result
             except (psycopg.InternalError, psycopg.OperationalError) as e:
                 msg = "\n".join([f"At {utils.current_timestamp()}", "For query:", str(query), "Message:", str(e)])
                 raise db.DatabaseServerError(msg, e)
             except psycopg.Error as e:
                 msg = "\n".join([f"At {utils.current_timestamp()}", "For query:", str(query), "Message:", str(e)])
                 raise db.DatabaseUserError(msg, e)
-            if cache_enabled:
-                self._inflate_query_cache()
-                self._query_cache[query] = query_result
 
         return _simplify_result_set(query_result)
 