b8b68d62771b2fcc74ce300ada36a67b1bdc91b0
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2024-08-15 20:25:29 +0200

    Create basic dependency graph abstraction for CEB predicate dependencies

--- postbound/postbound/util/misc.py
+++ postbound/postbound/util/misc.py
@@ -1,11 +1,15 @@
 """Contains various utilities that did not fit any other category."""
 from __future__ import annotations
 
+import collections
 import re
+from collections.abc import Iterable
 from datetime import datetime
-from typing import Any
+from typing import Any, Generator, Generic, Optional, TypeVar
 
 from postbound.util import jsonize
+
+T = TypeVar("T")
 
 
 def current_timestamp() -> str:
@@ -97,3 +101,60 @@ class Version(jsonize.Jsonizable):
 
     def __str__(self) -> str:
         return "v" + ".".join(str(v) for v in self._version)
+
+
+class DependencyGraph(Generic[T]):
+    """A simple dependency graph abstraction. Entries are added via `add_task` and iteration yields the source nodes first."""
+
+    def __init__(self) -> None:
+        self._source_nodes: set[int] = set()
+        self._dependencies: dict[int, list[int]] = collections.defaultdict(list)
+        self._nodes: dict[int, T] = {}
+
+    def add_task(self, node: T, *, depends_on: Optional[Iterable[T]] = None) -> None:
+        """Queues a new task/entry/whatever.
+
+        Parameters
+        ----------
+        node : T
+            The new task
+        depends_on : Optional[Iterable[T]], optional
+            Optional other tasks that have to be completed before this one. If those task have not been added yet, this will
+            be done automatically.
+        """
+        node_id = hash(node)
+        self._nodes[node_id] = node
+
+        if not depends_on:
+            self._source_nodes.add(node_id)
+            return
+
+        if node_id in self._source_nodes:
+            self._source_nodes.remove(node_id)
+
+        for dep in depends_on:
+            dep_id = hash(dep)
+            self._dependencies[dep].append(node_id)
+
+            if dep_id not in self._nodes:
+                self._source_nodes.add(dep_id)
+                self._nodes[dep_id] = dep
+
+    def __iter__(self) -> Generator[T, Any, None]:
+        provided_nodes: set[int] = set()
+        for node_id in self._source_nodes:
+            yield self._nodes[node_id]
+
+            provided_nodes.add(node_id)
+            dependency_stack = list(self._dependencies[node_id])
+
+            while dependency_stack:
+                dep_id = dependency_stack.pop()
+                if dep_id in provided_nodes:
+                    continue
+
+                yield self._nodes[dep_id]
+
+                provided_nodes.add(dep_id)
+                dependency_stack.extend(self._dependencies[dep_id])
+        provided_nodes = set()
