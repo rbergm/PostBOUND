622eb9eea1f7bf68f8e72a725ab8b0a1680fe621
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2024-09-23 16:15:06 +0200

    Rework SSB setup script
    Now supports the same arguments as the other workload scripts.
    We no longer rely on submodules. Instead, all required dependencies are
    automatically created when setting up the input data directory.
    
    Furthermore, the handling of existing data files is much smarter now:
    the input data directory contains subdirectories for each potential
    scale factor. When creating a new database, these directories will be
    re-used if possible.

--- postgres/workload-ssb-setup.sh
+++ postgres/workload-ssb-setup.sh
@@ -2,6 +2,15 @@
 
 set -e  # exit on error
 
+WD=$(pwd)
+DB_NAME="ssb"
+FORCE_CREATION="false"
+TARGET_DIR="../ssb_data"
+PG_CONN="-U $USER"
+SKIP_EXTENSIONS="false"
+SKIP_VACUUM="false"
+SF=10
+
 attempt_pg_ext_install() {
     EXTENSION=$1
     AVAILABLE_EXTS=$(psql $DB_NAME -t -c "SELECT name FROM pg_available_extensions" | grep "$EXTENSION" || true)
@@ -12,48 +21,128 @@ attempt_pg_ext_install() {
     psql $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS $EXTENSION;"
 }
 
-# use custom TPCH scale factor if supplied
-if [ -z "$1" ] ; then
-    SF=10
-else
-    SF=$1
+show_help() {
+    RET=$1
+    echo "Usage: $0 <options>"
+    echo "Allowed options:"
+    echo -e "-d | --dir\t\t<directory> specifies the directory to store/load the stats data files, defaults to '../ssb_data'"
+    echo -e "-s | --scale-factor\t<scale factor> specifies the scale factor to use (defaults to 10)"
+    echo -e "-f | --force\t\tdelete existing instance of the database if necessary"
+    echo -e "-t | --target\t\t<db name> name of the ssb database, defaults to 'ssb'"
+    echo -e "--pg-conn\t\t<connection> connection string to the PostgreSQL server (server, port, user) if required, e.g. '-h localhost -U admin'"
+    echo -e "--no-ext\t\tdoes not install any extensions"
+    echo -e "--no-vacuum\t\tdoes not run VACUUM ANALYZE after the import"
+    exit $RET
+}
+
+while [ $# -gt 0 ] ; do
+    case $1 in
+        -d|--dir)
+            TARGET_DIR=$2
+            shift
+            shift
+            ;;
+        -s|--scale-factor)
+            SF=$2
+            shift
+            shift
+            ;;
+        -f|--force)
+            FORCE_CREATION="true"
+            shift
+            ;;
+        -t|--target)
+            DB_NAME=$2
+            shift
+            shift
+            ;;
+        --pg-conn)
+            PG_CONN=$2
+            shift
+            shift
+            ;;
+        --no-ext)
+            SKIP_EXTENSIONS="true"
+            shift
+            ;;
+        --no-vacuum)
+            SKIP_VACUUM="true"
+            shift
+            ;;
+        --help)
+            show_help 0
+            ;;
+        *)
+            show_help 1
+            ;;
+    esac
+done
+
+EXISTING_DBS=$(psql $PG_CONN -l | grep "$DB_NAME" || true)
+
+echo ".. Working directory is $WD"
+
+if [ ! -z "$EXISTING_DBS" ] && [ $FORCE_CREATION = "false" ] ; then
+    echo ".. SSB exists, doing nothing"
+    exit 0
 fi
 
-root=$(pwd)
+if [ ! -z "$EXISTING_DBS" ] ; then
+    dropdb $PG_CONN $DB_NAME
+fi
 
-echo ".. Building SSB utility"
-patch -fs ssb-kit/dbgen/bm_utils.c ../util/ssb_dbgen.patch || true
-cd ssb-kit/dbgen
-make MACHINE=LINUX DATABASE=POSTGRESQL
+echo ".. SSB source directory is $TARGET_DIR"
 
-cd $root
-echo ".. Setting up environment"
-export DSS_CONFIG=$(pwd)/ssb-kit/dbgen
-export DSS_QUERY=$DSS_CONFIG/queries
-export DSS_PATH=../ssb_data
+if [ -d "$TARGET_DIR/ssb_data_$SF" ] ; then
+    echo ".. Re-using existing SSB input data"
+else
+    echo ".. SSB source directory does not exist, re-creating"
+    cd $TARGET_DIR
+    TARGET_DIR=$(pwd)  # get absolute path -- resolves relative target dirs
+    mkdir -p "$TARGET_DIR/ssb_data_$SF"
 
-echo ".. Generating SSB data (SF = $SF)"
-mkdir -p ../ssb_data
-ssb-kit/dbgen/dbgen -vf -s $SF -T a
+    if [ ! -d "$TARGET_DIR/ssb-kit" ] ; then
+        echo ".. Building SSB utility"
+        git clone https://github.com/gregrahn/ssb-kit.git
+        patch -fs ssb-kit/dbgen/bm_utils.c ../util/ssb_dbgen.patch || true
+        cd ssb-kit/dbgen
+        make MACHINE=LINUX DATABASE=POSTGRESQL
+    fi
+
+    echo ".. Setting up environment"
+    cd $TARGET_DIR
+    export DSS_CONFIG="$TARGET_DIR/ssb-kit/dbgen"
+    export DSS_QUERY="$DSS_CONFIG/queries"
+    export DSS_PATH="$TARGET_DIR/ssb_data_$SF"
+    sed -i "4s#.*#\\\set path '$TARGET_DIR/ssb_data_$SF/'#" ssb-kit/scripts/pg_load.sql
+
+    echo ".. Generating SSB data (SF = $SF)"
+    ssb-kit/dbgen/dbgen -vf -s $SF -T a
+fi
 
 echo ".. Creating SSB database schema"
-createdb ssb
+createdb $PG_CONN $DB_NAME
 attempt_pg_ext_install "pg_buffercache"
 attempt_pg_ext_install "pg_prewarm"
 attempt_pg_ext_install "pg_cooldown"
 attempt_pg_ext_install "pg_hint_plan"
 
-wget -nv --output-document ../ssb_data/pg_schema.sql https://raw.githubusercontent.com/gregrahn/ssb-kit/master/scripts/pg_schema.sql
-psql ssb -f ../ssb_data/pg_schema.sql
+echo ".. Loading SSB database schema"
+cd $TARGET_DIR
+psql $PG_CONN $DB_NAME -f $TARGET_DIR/ssb-kit/scripts/pg_schema.sql
 
 echo ".. Inserting SSB data into database"
-wget -nv --output-document ../ssb_data/pg_load.sql https://raw.githubusercontent.com/gregrahn/ssb-kit/master/scripts/pg_load.sql
-
-# this patch may never fail since we just downloaded the live version (unless this version is changed)
-patch ../ssb_data/pg_load.sql ../util/ssb_pg_load.patch
-psql ssb -f ../ssb_data/pg_load.sql
+psql $PG_CONN $DB_NAME -f $TARGET_DIR/ssb-kit/scripts/pg_load.sql
 
 echo ".. Creating SSB Foreign Key indices"
-psql ssb -f workload-ssb-fk-indexes.sql
+psql $PG_CONN $DB_NAME -f $WD/workload-ssb-fk-indexes.sql
 
-psql $DB_NAME -c "VACUUM ANALYZE;"
+if [ $SKIP_VACUUM == "false" ] ; then
+    echo ".. Vacuuming database"
+    psql $PG_CONN $DB_NAME -c "VACUUM ANALYZE;"
+else
+    echo ".. Skipping vacuuming"
+fi
+
+echo ".. Done"
+cd $WD
