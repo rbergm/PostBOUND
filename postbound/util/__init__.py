57f6a61065064b891e137964baf8d759748972e4
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2025-10-23 13:38:03 +0200

    Expose some more commonly-used utilities directly in pb.util

--- postbound/util/__init__.py
+++ postbound/util/__init__.py
@@ -5,8 +5,8 @@ from . import networkx as nx
 from ._errors import InvariantViolationError, LogicError, StateError
 from .collections import enlist, flatten, powerset, set_union, simplify
 from .dicts import argmin, frozendict, hash_dict
-from .jsonize import jsondict, to_json
-from .logging import make_logger, timestamp
+from .jsonize import jsondict, to_json, to_json_dump
+from .logging import Logger, make_logger, timestamp
 from .misc import DependencyGraph, Version, camel_case2snake_case
 from .proc import run_cmd
 from .stats import jaccard
@@ -28,8 +28,10 @@ __all__ = [
     "InvariantViolationError",
     "jsondict",
     "to_json",
+    "to_json_dump",
     "timestamp",
     "make_logger",
+    "Logger",
     "camel_case2snake_case",
     "Version",
     "DependencyGraph",
